6612b1cb265bd929b9315e82ff3c2683e47a375f
Cherch <b.cherchiglia@gmail.com>
2023-06-09 15:08:49 -0400

    steps 6 and 7 completed - routes, templates and views

--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,9 @@ def home(request):
     # Step 6.1 include a .html file extension to HOME
     # to return to home page
     return render(request, 'home.html')
+
+# Step 7.0 mkdir main_app/templates 
+# to CREATE a TEMPLATES DIRECTORY for all template files 
+# (will always be html in Django)
+
+# Step 7.1 create a home.html file in the GUI and go there
