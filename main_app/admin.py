d40bb6d114fd52fb4293d6952df35c07795d1185
Cherch <b.cherchiglia@gmail.com>
2023-06-14 21:11:45 -0400

    admin feature restored for using

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Import your models here
-from .models import Guitar
+from .models import Guitar, Using
 
 # Register your models here.
 admin.site.register(Guitar)
+admin.site.register(Using)
