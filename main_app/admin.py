cb9ae5794ccb0b95e9811c4692f86655831a4e91
Cherch <b.cherchiglia@gmail.com>
2023-06-14 20:07:27 -0400

    Custom Field Label Added

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Import your models here
-from .models import Guitar
+from .models import Guitar, Using
 
 # Register your models here.
 admin.site.register(Guitar)
+# Lab pt 4 register USING Class
+admin.site.register(Using)
